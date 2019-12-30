5e18f58b16472d9baa0cc92aabe2f3cadf09997c
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-12-30 20:23:38 +0300

    Fix tests for Windows

--- packages/graphql-tag-pluck/tests/utils/tmp.ts
+++ packages/graphql-tag-pluck/tests/utils/tmp.ts
@@ -1,4 +1,5 @@
 import { file, FileCallback } from 'tmp'
+import { ensureFileSync } from 'fs-extra';
 
 export const createTmpFile = (options: any) => new Promise<{ name: string, cleanupCallback: Function }>((resolve, reject) => {
   const callback: FileCallback = (err: Error, filename: string, cleanupCallback: any) => {
@@ -6,12 +7,12 @@ export const createTmpFile = (options: any) => new Promise<{ name: string, clean
       reject(err)
     }
     else {
+      ensureFileSync(filename);
       resolve({
         cleanupCallback,
         name: filename,
       })
     }
   }
-
   file(options, callback)
 })
